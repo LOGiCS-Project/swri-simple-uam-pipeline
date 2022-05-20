ff73fb899192c21600d1af633bb78affdaee7c3d
Rohit Ramesh <rkr@berkeley.edu>
2022-05-19 18:19:38 -0700

    Updating templates to match other code and be safer.

--- tasks.py
+++ tasks.py
@@ -2,6 +2,10 @@
 
 from invoke import task
 import copier
+import tempfile
+import shutil
+from pathlib import Path
+
 
 @task
 def update_conda(c):
@@ -9,4 +13,17 @@ def update_conda(c):
 
 @task
 def new_subpackage(c):
-    copier.copy("subpackage-template",".",cleanup_on_error=False)
+    """
+    Creates a new subpackage dir, manually moving stuff around because
+    Copier's error recovery will clobber everything.
+    """
+    temp_dir = tempfile.gettempdir()
+    dist_file = 'dist_name.txt'
+    print(f"Unpacking template to: {temp_dir}")
+    copier.copy("subpackage-template",temp_dir,cleanup_on_error=False)
+
+    sub_dir = (Path(temp_dir) / dist_file).read_text()
+    target_dir = Path(__file__).parent /  sub_dir
+    print(f"Moving template to: {target_dir}")
+    shutil.move(temp_dir, target_dir)
+    (target_dir / dist_file).unlink()
