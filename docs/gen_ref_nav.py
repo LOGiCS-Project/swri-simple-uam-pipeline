4e0fc54cc3c888358cc36ff1e8f23593f18ef1ab
Rohit Ramesh <rkr@berkeley.edu>
2022-05-30 14:26:03 -0700

    Trimming unused documentation files

--- docs/gen_ref_nav.py
+++ docs/gen_ref_nav.py
@@ -6,27 +6,37 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("src").rglob("*.py")):
-    module_path = path.relative_to("src").with_suffix("")
-    doc_path = path.relative_to("src").with_suffix(".md")
-    full_doc_path = Path("reference", doc_path)
+packages = {
+    "simple-uam": {"src_dir": "simple_uam/"},
+    "util": {"src_dir": "subpackages/util/util/"},
+}
 
-    parts = tuple(module_path.parts)
+for package, data in packages.items():
+    src_dir = Path(data['src_dir'])
 
-    if parts[-1] == "__init__":
-        parts = parts[:-1]
-        doc_path = doc_path.with_name("index.md")
-        full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] == "__main__":
-        continue
+    for path in sorted(src_dir.rglob("*.py")):
 
-    nav[parts] = doc_path.as_posix()
+        module_path = path.relative_to(src_dir.parent).with_suffix("")
+        doc_path = path.relative_to(src_dir.parent).with_suffix(".md")
+        full_doc_path = Path("reference", doc_path)
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        ident = ".".join(parts)
-        fd.write(f"::: {ident}")
+        parts = tuple(module_path.parts)
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, path)
+        if parts[-1] == "__init__":
+            parts = parts[:-1]
+            doc_path = doc_path.with_name("index.md")
+            full_doc_path = full_doc_path.with_name("index.md")
+        elif parts[-1] == "__main__":
+            continue
+
+        nav[parts] = doc_path.as_posix()
+
+
+        with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+            ident = ".".join(parts)
+            fd.write(f"::: {ident}")
+
+        mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
 # add pages manually:
 # nav["package", "module"] = "path/to/file.md"
