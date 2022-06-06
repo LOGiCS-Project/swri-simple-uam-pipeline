177e629cc040c25258f8c3c37c40b1475eb74540
Rohit Ramesh <rkr@berkeley.edu>
2022-06-06 12:14:14 -0700

    Taking notes on running a gremlin graphserver.

--- src/simple_uam/util/system/__init__.py
+++ src/simple_uam/util/system/__init__.py
@@ -1,5 +1,6 @@
 from .backup import backup_file, archive_files
 from .rsync import Rsync
+from .git import Git
 # We don't import '.windows' so that you have to import platform specific stuff
 # manually.
 
@@ -9,4 +10,5 @@ __all__: List[str] = [
     'backup_file',
     'archive_files',
     'Rsync',
+    'Git',
 ]  # noqa: WPS410 (the only __variable__ we use)
